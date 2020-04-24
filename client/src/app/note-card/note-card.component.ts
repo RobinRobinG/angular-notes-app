35bb63c8fc2b8a5a809e10e3ca17840e105e3a58
Robin Guan <robinguan@MacBook-Pro.local>
2020-04-24 01:56:52 -0700

    first commit, working on the notes list page

---
+++ client/src/app/note-card/note-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, AfterViewInit, ViewChild, ElementRef, Renderer2, Input } from '@angular/core';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-note-card',
+  templateUrl: './note-card.component.html',
+  styleUrls: ['./note-card.component.scss']
+})
+export class NoteCardComponent implements AfterViewInit {
+  faTrashAlt = faTrashAlt;
+
+  @Input('title') title: string;
+  @Input('body') body: string;
+
+  @ViewChild('truncator') truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText') bodyText: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    const style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    const viewableHeight = parseInt(style.getPropertyValue('height'), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
+}
